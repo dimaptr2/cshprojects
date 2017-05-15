3b7cfec8e96b82add776ccdd7a5e6c994772577a
Петров <DPetrov@eatmeat.ru>
2017-05-15 15:36:26 +0300

    New CJ

---
+++ CashJournal/CashJournal/StartUI.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace CashJournal
+{
+    partial class StartUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.commandBox = new System.Windows.Forms.GroupBox();
+            this.atDate = new System.Windows.Forms.DateTimePicker();
+            this.btnZ = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.commandBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // commandBox
+            // 
+            this.commandBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.commandBox.Controls.Add(this.btnRead);
+            this.commandBox.Controls.Add(this.btnZ);
+            this.commandBox.Controls.Add(this.atDate);
+            this.commandBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.commandBox.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.commandBox.Location = new System.Drawing.Point(26, 12);
+            this.commandBox.Name = "commandBox";
+            this.commandBox.Size = new System.Drawing.Size(267, 495);
+            this.commandBox.TabIndex = 1;
+            this.commandBox.TabStop = false;
+            this.commandBox.Text = "Печать приходных ордеров";
+            // 
+            // atDate
+            // 
+            this.atDate.Location = new System.Drawing.Point(6, 32);
+            this.atDate.Name = "atDate";
+            this.atDate.Size = new System.Drawing.Size(255, 22);
+            this.atDate.TabIndex = 0;
+            // 
+            // btnZ
+            // 
+            this.btnZ.Location = new System.Drawing.Point(6, 60);
+            this.btnZ.Name = "btnZ";
+            this.btnZ.Size = new System.Drawing.Size(255, 36);
+            this.btnZ.TabIndex = 1;
+            this.btnZ.Text = "Z-отчёт";
+            this.btnZ.UseVisualStyleBackColor = true;
+            this.btnZ.Click += new System.EventHandler(this.btnZ_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(6, 102);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(255, 36);
+            this.btnRead.TabIndex = 2;
+            this.btnRead.Text = "Получить ПО";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // StartUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 519);
+            this.Controls.Add(this.commandBox);
+            this.Name = "StartUI";
+            this.Text = "Кассовая программа";
+            this.Load += new System.EventHandler(this.StartUI_Load);
+            this.commandBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox commandBox;
+        private System.Windows.Forms.DateTimePicker atDate;
+        private System.Windows.Forms.Button btnZ;
+        private System.Windows.Forms.Button btnRead;
+    }
+}
---
+++ CashJournal/CashJournal/StartUI.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CashJournal
+{
+    public partial class StartUI : Form
+    {
+        private Dictionary<string, string> sapParams;
+
+        public StartUI()
+        {
+            InitializeComponent();
+            sapParams = new Dictionary<string, string>();
+        }
+
+        private void StartUI_Load(object sender, EventArgs e)
+        {
+            DateTime currentDate = DateTime.Now;
+            atDate.Text = currentDate.ToString();
+            readLogonFile("C:\\Users\\DPETROV\\CJLogon\\logon.txt");
+        }
+
+        private void readLogonFile(string fileName)
+        {
+            string line;
+            
+            StreamReader file =
+                            new StreamReader(fileName);
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] separator = new string[1] { "=" };
+                string[] result = line.Split(separator, StringSplitOptions.None);
+                if (result.Length == 2)
+                {
+                    sapParams[result[0]] = result[1];
+                }
+            }
+            file.Close();
+        }
+
+        // Get Z-Report from the gadget
+        private void btnZ_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
